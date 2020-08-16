24a63dbdd68f6dbcb5cbdb06a5461f33db3aee4d
GrendGreen <65673679+GrendGreen@users.noreply.github.com>
2020-08-16 11:09:06 +0300

    Add files via upload

---
+++ Site/script.js
@@ -0,0 +1,16 @@
+﻿jQuery('document').ready(function() { 
+
+                 var gotop_scroll_start = 200;
+		function gotop_scroll(){
+    	if($(window).scrollTop()<gotop_scroll_start) $('.gototop').fadeOut(400);
+    	else $('.gototop').fadeIn(300);
+		}
+			$(function(){
+    			$('body').append('<div class="gototop" title="Наверх &uarr;"></div>');
+   				 if($(window).scrollTop()>gotop_scroll_start) $('.gototop').show();
+    			$(window).scroll(gotop_scroll);
+    		$(".gototop").click(function(){$('html,body').animate({scrollTop:0})});
+		})
+
+
+ });         
